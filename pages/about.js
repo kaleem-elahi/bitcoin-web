5116bb81d6781610d3f847ae1c4674a6e50d9b81
kaleem-elahi <shaikh9996@gmail.com>
2019-01-09 11:46:05 +0530

    addd AboutMe section

--- pages/about.js
+++ pages/about.js
@@ -3,8 +3,17 @@ import Layout from '../components/Layout';
 const About = () => (
 <Layout>
 <div>
+  <h1>About Me</h1>
+  <p>Hi! 🙋🏻‍♂️</p>
+  <p>This repo is made by Kaleem Elahi.</p>
+  <div> Who is 22yo web developer and works at a StartUp based in Ahmedabad, India.</div>
+  <br/>
+  <div>Check his profile: <a href="https://gitshowcase.com/kaleem-elahi">Kaleem Elahi</a></div>
+  <br/>
   <h1>About BitzPrice</h1>
   <p>Application to view Bitcoin prices</p>
+  <p>The app is made from this Tutorial</p>
+  <p>The app is made just for learning purpose.</p>
 </div>
 </Layout>
 );
